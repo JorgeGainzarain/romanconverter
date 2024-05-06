aac310819b787347e31e23f36c9cbaddeea8e92d
JorgeGainzarain <118836019+JorgeGainzarain@users.noreply.github.com>
2024-05-06 17:12:46 +0200

    Update romans.js

--- romans.js
+++ romans.js
@@ -197,9 +197,6 @@ function init() {
         return obj[1000];
       }
 
-      module.exports.convertRomanToInteger = convertRomanToInteger;
-      module.exports.convertIntegerToRoman = convertIntegerToRoman;
-
       return obj[1000].repeat(parseInt(num / 1000));
     }
   }
