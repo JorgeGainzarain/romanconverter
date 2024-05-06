2f76edcc53a47f62419760a35f8c67557a19b6cb
JorgeGainzarain <118836019+JorgeGainzarain@users.noreply.github.com>
2024-05-06 15:35:53 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,24 @@
-
+// .eslintrc.js
+module.exports = {
+  "env": {
+    "commonjs": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": ["eslint:recommended"],
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "asyncFunctions": true,
+      "classes": true,
+      "jsx": true
+    }
+  },
+  "rules": {
+    "no-console": "error",
+    "no-undef": "error",
+    "no-unused-vars": "error",
+    "no-irrelevant-methods": "error"
+  }
+};
