b4690342e55297a107687ad63adbc2d61056abf2
JorgeGainzarain <118836019+JorgeGainzarain@users.noreply.github.com>
2024-05-07 14:29:44 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    // Enforce the use of strict mode
+    'strict': ['error', 'global'],
+
+    // Enforce semicolons at the end of statements
+    'semi': ['error', 'always'],
+
+    // Enforce single quotes for string literals
+    'quotes': ['error', 'single'],
+
+    // Require a space before function parenthesis
+    'space-before-function-paren': ['error', 'always'],
+
+    // Disallow trailing whitespace at the end of lines
+    'no-trailing-spaces': 'error',
+
+    // Enforce consistent indentation (use 2 spaces)
+    'indent': ['error', 2],
+
+    // Enforce consistent spacing inside braces
+    'object-curly-spacing': ['error', 'always'],
+
+    // Enforce consistent spacing before blocks
+    'space-before-blocks': ['error', 'always'],
+  },
+};
